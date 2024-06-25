7ef3d69f4813bd8d8d079b13fa0d056eaa51e80d
Sena Nur BAYIR <senanurbayir13@gmail.com>
2024-06-25 23:44:38 +0300

    checkbox and errorOverlay are added

---
+++ src/components/ui/Checkbox.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { horizontalScale } from "../../constants/scaling";
+import IconButton from "./IconButton";
+
+const Checkbox = ({ isSelected, handleSelect }) => {
+  return (
+    <View style={[styles.checkbox, isSelected && styles.selectedCheckbox]}>
+      {isSelected && (
+        <IconButton
+          name="checkmark-sharp"
+          size={horizontalScale(15)}
+          color="white"
+          onPress={handleSelect}
+        />
+      )}
+    </View>
+  );
+};
+
+export default Checkbox;
+
+const styles = StyleSheet.create({
+  checkbox: {
+    width: horizontalScale(20),
+    height: horizontalScale(20),
+    backgroundColor: "white",
+    marginRight: horizontalScale(10),
+    alignSelf: "center",
+    justifyContent: "center",
+    borderRadius: horizontalScale(4),
+    padding: horizontalScale(2),
+    borderWidth: 1,
+    borderColor: "#797979",
+  },
+  selectedCheckbox: {
+    backgroundColor: "#0075ff",
+    borderColor: "#0075ff",
+  },
+});
---
+++ src/components/ui/ErrorOverlay.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from "../../constants/scaling";
+
+const ErrorOverlay = ({ message }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{message}</Text>
+    </View>
+  );
+};
+
+export default ErrorOverlay;
+
+const styles = StyleSheet.create({
+  container: {
+    height: verticalScale(100),
+    backgroundColor: "#f8fafc",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderRadius: horizontalScale(10),
+    borderColor: "#94a3b8",
+  },
+  text: {
+    color: "#4b596c",
+    fontWeight: "bold",
+    fontSize: scaleFontSize(15),
+  },
+});
